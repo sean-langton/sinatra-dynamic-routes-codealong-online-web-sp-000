03574574c8667729f8c50a42a6481740df1c8044
Sean Langton <slangton1@gmail.com>
2019-01-29 22:55:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,9 +19,9 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get "/multiply/:num1/:num2" do
+  get "/multiply" do
     @result = params[:num1]*params[:num2]
     "#{@result}"
   end
-  
+
 end
